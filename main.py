4efb8f3b74c710055ddc992824f57a6ac0ad443b
akitorahayashi <atora.has75@gmail.com>
2025-07-27 16:24:01 +0900

    input、outputディレクトリを追加

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+
 import glob
+import shutil
+import os
 from custom_operation import custom_operation
 
 def find_excel_file():
     """
-    Finds an Excel file in the current directory and returns its name.
+    Finds an Excel file in the 'input' directory and returns its name.
     """
-    excel_files = glob.glob("*.xlsx")
+    excel_files = glob.glob("input/*.xlsx")
     if len(excel_files) == 1:
         return excel_files[0]
     else:
@@ -16,6 +19,8 @@ if __name__ == "__main__":
     if excel_file:
         print(f"Excelファイルが見つかりました: '{excel_file}'")
         processed_file = custom_operation(excel_file)
-        print("処理が完了しました。")
+        output_file = os.path.join("output", os.path.basename(processed_file))
+        shutil.copy(processed_file, output_file)
+        print(f"処理が完了し、'{output_file}' に保存されました。")
     else:
         print("Excelファイルが見つかりませんでした。")
